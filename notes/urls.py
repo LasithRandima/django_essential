109f6870cc4526c74bc7400d123030f098395629
Lasith Randima <lasithrandima123@gmail.com>
2024-05-26 05:57:50 +0530

    Styling & navigating

--- notes/urls.py
+++ notes/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     # path('notes', views.list),
-    path('notes', views.NotesListView.as_view()), # class based view
+    path('notes', views.NotesListView.as_view(), name='notes_list'), # class based view
     # path('notes/<int:pk>', views.detail),
-    path('notes/<int:pk>', views.NotesDetailView.as_view()), # class based view
+    path('notes/<int:pk>', views.NotesDetailView.as_view(), name="notes.detail"), # class based view
 ]
